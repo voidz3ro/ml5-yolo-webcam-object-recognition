a39a65f1e04a1cfae51828a8958e668db3801bd8
Tristan Bullard <tristan.bullard@gmail.com>
2019-04-12 12:52:55 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,75 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import * as ml5 from 'ml5'
+import * as p5 from 'p5'
 
 class App extends Component {
+  yolo
+  video
+  status
+  objects = []
+
+  constructor() {
+    super()
+    this.state = {
+      status: 'Loading...'
+    }
+  }
+
+  componentDidMount() {
+    this.setup()
+  }
+
+  setup() {
+    p5.createCanvas(800, 600)
+    this.video = p5.createCapture('VIDEO')
+    this.video.size(800, 600)
+
+    this.yolo = ml5.YOLO(this.video, this.beginDetection)
+
+    this.video.hide()
+    this.status = p5.select('#status')
+  }
+
+  beginDetection() {
+    console.log(`Model loaded bro`)
+    this.detect()
+  }
+
+  draw() {
+    let width = 800
+    let height = 600
+    p5.image(this.video, 0, 0, width, height)
+    this.objects.forEach((obj, i) => {
+      p5.noStroke()
+      p5.fill(0, 255, 0)
+      p5.text(obj[i].className, obj[i].x * width, obj[i].y * height - 5)
+      p5.noFill()
+      p5.strokeWeight(4)
+      p5.stroke(0, 255, 0)
+      p5.rect(
+        obj[i].x * width,
+        obj[i].y * height,
+        obj[i].w * width,
+        obj[i].h * height
+      )
+    })
+  }
+
+  detect() {
+    this.yolo.detect((err, results) => {
+      this.objects = results
+      this.detect()
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div id="status">meh!</div>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
